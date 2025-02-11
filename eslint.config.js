ed04c98e91fb21e3a4746f1e1f67c0c3d21d0100
Mael Abgrall <172921326+Mael-Abgrall@users.noreply.github.com>
2025-02-11 16:53:35 +0000

    update packages

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,4 @@
 import astarEslint from '@ansearch/config/linters/eslint.config.js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  ...astarEslint,
-  {
-    ignores: ['.prettierrc*'],
-    rules: {
-      'sonarjs/no-empty-test-file': 0, // duplicate of no-warning-comments
-    },
-  },
-  {
-    // disable some rules in test files
-    files: ['**/tests/**'],
-    rules: {
-      'security/detect-non-literal-fs-filename': 0,
-      'eslint/security/detect-non-literal-fs-filename': 0,
-    },
-  },
-);
+export default tseslint.config(...astarEslint);
